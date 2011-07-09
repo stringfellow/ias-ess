71ac3bbf202f990da0b120818f283f1ee6955864
Steve Pike <steve@synfinity.net>
2011-07-09 18:09:37 +0100

    some urls

--- ias/urls.py
+++ ias/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls.defaults import *
 
+
+
 urlpatterns = patterns('',
     ('^sighting/$',
      'django.views.generic.simple.direct_to_template',
      {'template': 'ias/sighting.html'}
      ),
+    ('^taxon/register/$',
+        'views.register_taxon',
+        name="register_species")
 )
