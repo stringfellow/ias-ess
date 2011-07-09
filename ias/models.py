3d3daac98a30071e3eb5a99e5dbf76936d4bee13
Steve Pike <steve@terciel.config>
2011-07-09 16:05:09 +0100

    added core 'Taxa' table

--- ias/models.py
+++ ias/models.py
@@ -1,3 +1,26 @@
+"""Models for IAS app - Species and Sightings."""
 from django.db import models
 
-# Create your models here.
+
+TAXA_CHOICES = (
+    ('SPECIES', 'Species'),
+    ('GENUS', 'Genus'),
+    ('FAMILY', 'Family'),
+    ('ORDER', 'Order'),
+    ('CLASS', 'Class'),
+    ('PHYLUM', 'Phylum'),
+    ('KINGDOM', 'Kingdom'),
+    ('DOMAIN', 'Domain'),
+)
+
+class Taxon(models.Model):
+    """A thing to sight, could be a group or a single species."""
+    scientific_name = models.CharField(max_length=200)
+    rank = models.CharField(choices=TAXA_CHOICES, max_length=50)
+    key_text = models.TextField(help_text="A helpful description so that users"
+        " can identify this taxon easily.")
+    questionnaire = models.URLField(help_text="Where is the Google"
+        " Questionnaire to which users should fully answer their sighting.")
+
+
+
