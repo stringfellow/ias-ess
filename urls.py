283ca52b63510675616d07d368ded61c692e8588
Mark <mark@mark-laptop.(none)>
2011-07-09 16:04:23 +0100

    bootstrapped ias app

--- urls.py
+++ urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     ('^$', 'django.views.generic.simple.direct_to_template',
      {'template': 'home.html'}),
+    ('^ias/', include('ias.urls'))
 )
-
