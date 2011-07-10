aea71c471c5155ff9a372d9693932457aab4c50d
Martin Allison <martinallison@gmail.com>
2011-07-10 18:06:35 +0100

    Added generic list view for sightings

--- ias/urls.py
+++ ias/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls.defaults import *
+from django.views.generic import ListView
+
+from ias.models import Sighting
 
 
 urlpatterns = patterns('',
     url('^sighting/$',
         'ias.views.sighting',
         name="ias-add-sighting",
-     ),
+    ),
     url('^taxon/register/$',
         'ias.views.register_taxon',
         name="ias-register-taxon"
@@ -13,5 +16,9 @@ urlpatterns = patterns('',
     url('^sighting/(\d+)$',
         'ias.views.sighting_detail',
         name='ias-sighting-detail'
+    ),
+    url('^sightings/$',
+        ListView.as_view(model=Sighting),
+        name='ias-sighting-list'
     )
 )
