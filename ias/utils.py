2b0f2ca7ee7c3c028b3e5b36afad1b7ecd079f73
Steve Pike <steve@synfinity.net>
2011-07-11 21:23:09 +0100

    regex for form working with basic etree... ugh

--- ias/utils.py
+++ ias/utils.py
@@ -1,4 +1,5 @@
 import re
+import logging
 from xml.etree import ElementTree as ET 
 import urllib2
 
@@ -8,33 +9,34 @@ def tweak_google_form(questionnaire_id, sighting_pk):
     url = GOOGLE_QA_URL + questionnaire_id
     response = urllib2.urlopen(url)
     data = response.read()
+    data = data.replace('>', '>\n')  # why? Who knows!
     data = re.sub(r'<br>', r'<br/>', data)
     data = re.sub(r'<input(.*?)>', r'<input\1/>', data)
     pat = re.compile(r'(<form.*?\/form>)', re.DOTALL)
-    match = re.match(pat, data)
+    match = re.search(pat, data)
     if not match:
-        import pdb
-        pdb.set_trace()
+        logging.warn("Form failed to match regex for key %s" % (
+            questionnaire_id))
+        return ""
     form = ET.fromstring(match.group(0))
     # find the input 0 - HOPE this is the ref...
-    ref = form.find(".//input[@id='entry_0']")
-    import pdb
-    pdb.set_trace()
-    ref['value'] = sighting_pk
-    ref['type'] = 'hidden'
+    
+    ref = form.find(".//input")
+    ref.set('type', 'hidden')
+    ref.set('value', str(sighting_pk))
     # now extract its previous siblings
-    sibs = []
-    sib = ref.previousSibling
-    sibs.append(sib)
-    while sib.previousSibling != None:
-        sib = sib.previousSibling
-        sibs.append(sib)
-
-    [s.extract() for s in sibs]
+#    sibs = []
+#    sib = ref.previousSibling
+#    sibs.append(sib)
+#    while sib.previousSibling != None:
+#        sib = sib.previousSibling
+#        sibs.append(sib)
+#
+#    [s.extract() for s in sibs]
 
     # prepare form for our hackety redirect
     # see: http://www.morningcopy.com.au/tutorials/how-to-style-google-forms/
-    form['target'] = "hidden_iframe"
-    form['onsubmit'] = "submitted=true;"
+    form.set('target', "hidden_iframe")
+    form.set('onsubmit', "submitted=true;")
 
-    return form.prettify()
+    return ET.tostring(form)
