93776d00c154411c7dce2aaed2e87264b2d34f3e
Steve Pike <steve@synfinity.net>
2011-07-21 21:52:46 +0100

    csrf for sighitng api

--- api/views.py
+++ api/views.py
@@ -1,10 +1,13 @@
 import logging
 
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.views.decorators.csrf import csrf_view_exempt
 
 from ias.forms import SightingForm
 
 
+@csrf_view_exempt
 def sighting(request):
     if request.method == 'POST':
         logging.debug("POST!")
@@ -13,6 +16,9 @@ def sighting(request):
         form = SightingForm(request.POST, request.FILES)
         if form.is_valid():
             logging.debug("VALID"*10)
+            sighting = form.save()
+            return HttpResponse("http://localhost:8080"+reverse(
+                "ias-sighting-detail", args=[sighting.pk]))
         else:
             logging.error(form.errors)
     else:
